cff23921e50a6f5c65b2cc02e0dba054707da509
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2023-08-06 20:01:26 +0300

    set_up FitnessApp

---
+++ lib/home/main_fitness_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:fitnessapp_flutter/main.dart';
+import 'package:fitnessapp_flutter/utils/color.dart';
+
+class MainFitenssPage extends StatefulWidget {
+  const MainFitenssPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFitenssPage> createState() => _MainFitenssPageState();
+}
+
+class _MainFitenssPageState extends State<MainFitenssPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
