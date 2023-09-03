83a2a8a3859d24b090223c1a56cb31c0457b01a9
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2023-09-03 19:03:31 +0300

    Third_Task

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home/main_fitness_page.dart';
-
-
+import 'registration_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +8,70 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      home: MyHomePage(title: 'Fitness App Flutter'),
+    );
+  }
+}
 
-        primarySwatch: Colors.blue,
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-      home: MainFitenssPage(),
+      body: const Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/image/FTCP_logo.png'),
+              backgroundColor: Colors.white,
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                )
+              ]),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login'),
+            ),
+            SizedBox(height: 10.0),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => RegistrationPage()));
+              },
+              child: Text('Register'),
+            )
+          ])),
     );
   }
 }
---
+++ lib/utils/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0x51000000);
+const buttonLineColor = Color(0x000C71A7);
+const titleColor = Color(0xFFFBFDFE);
+const headOneColor = Color(0xFFD3D5D6);
+const bodyColor = Color(0xAED3D5D6);
